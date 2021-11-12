f8839c59ed8cb77ea3b653da661076b9f057372d
J-Vostrikova <90725937+J-Vostrikova@users.noreply.github.com>
2021-11-12 12:30:03 +0300

    Update fun_cal.js

--- fun_cal.js
+++ fun_cal.js
@@ -1,23 +1,26 @@
-
-    let reg = /[A-Za-zА-Яа-я]/;
-    let weight = document.querySelector('#weight');
-    let pay = document.querySelector('#pay');
-    weight.oninput = function(){
-        this.value = this.value.replace(reg, '');    
+let reg = /[A-Za-zА-Яа-я]/;
+let weight = document.querySelector('#weight');
+let pay = document.querySelector('#pay');
+weight.oninput = function(){
+    this.value = this.value.replace(reg, '');    
+}
+pay.oninput = function(){
+    this.value = this.value.replace(reg, '');    
+}
+function calc() {
+    let cost;
+    let weight = document.getElementById('weight').value;
+    let pay = document.getElementById('pay').value;
+    if (pay == '' || weight == ''){
+        alert("Вы ввели не все данные");
+    } else if (pay <= 0 || weight <= 0){
+        alert("Введите корректные данные");
+    } else{
+        cost = weight * pay;
+        document.getElementById('cost').innerHTML = "Стоимость товара "+ cost +".00 руб.";
     }
-    pay.oninput = function(){
-        this.value = this.value.replace(reg, '');    
-    }
-    function calc() {
-        let cost;
-        let weight = document.getElementById('weight').value;
-        let pay = document.getElementById('pay').value;
-        if (pay == '' || weight == ''){
-            alert("Вы ввели не все данные");
-        } else if (pay <= 0 || weight <= 0){
-            alert("Введите корректные данные");
-        } else{
-            cost = weight * pay;
-            document.getElementById('cost').innerHTML = "Стоимость товара "+ cost +".00 руб.";
-        }
-    }
+}
+window.addEventListener('DOMContentLoaded', function (_event) {
+    console.log("DOM fully loaded and parsed");
+    document.getElementById("my-button").addEventListener("click", calc);
+  });
